774c5476012b5718796aa4047730c6b60e2d72ce
Curia Adrian <adrian.curia@metrosystems.net>
2017-07-13 13:05:33 +0300

    first commit

---
+++ src/KotlinTut.kt
@@ -0,0 +1,23 @@
+/**
+ * Created by adrian.curia on 7/10/2017.
+ */
+
+package demo
+
+import java.time.ZoneId
+
+fun  main(args : Array<String>) {
+    System.out.println("hello world")
+
+    val name = "Adi"
+    var age = 33
+
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+    var bigFloat: Float = Float.MAX_VALUE
+    var smallFloat: Float = Float.MIN_VALUE
+
+    System.out.println("BigInt is=" + bigInt)
+    System.out.println("SmallInt is= $smallInt")
+
+}
